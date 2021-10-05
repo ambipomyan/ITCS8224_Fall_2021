1cc09d77aada4d973639fdcd12947b3c9db588f8
Kewei Yan <ambipom_yan@outlook.com>
2021-10-05 13:15:26 -0400

    Update code
    - Add a simple skip-connection to network

--- DCGAN.py
+++ DCGAN.py
@@ -73,15 +73,24 @@ class Discriminator(Model):
         self.fc1 = layers.Dense(1024)
         self.bn3 = layers.BatchNormalization()
         self.fc2 = layers.Dense(2)
+        # 1*1 convolutional layer, for skip connection
+        self.conv_4_skip = layers.Conv2D(128, 1, stride=4, padding='SAME')
 
     # Set forward pass.
     def call(self, x, is_training=False):
         x = tf.reshape(x, [-1, 28, 28, 1])
+        # Copy tensor: expectind a 4-D dataset
+        y = x
+        #
         x = self.conv1(x)
         x = self.bn1(x, training=is_training)
         x = tf.nn.leaky_relu(x)
         x = self.conv2(x)
         x = self.bn2(x, training=is_training)
+        # Practice skip-connection: run 1*1 conv before addition
+        y = self.conv_4_skip(y)
+        x = layers.Add()[x, y]
+        #
         x = tf.nn.leaky_relu(x)
         x = self.flatten(x)
         x = self.fc1(x)
